ad69cac18901350484470ded455d417daa7553dd
Hongfei Yang <hongfei.yang@meeco.me>
2021-02-13 21:33:22 +1100

    initial commit

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -7,7 +7,7 @@ import mobileffmpeg
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(FFmpeg)
-public class FFmpeg: CAPPlugin, ExecuteDelegate {
+public class FFmpeg: CAPPlugin {
     
     private var execCall: CAPPluginCall?
     
@@ -21,31 +21,40 @@ public class FFmpeg: CAPPlugin, ExecuteDelegate {
     @objc func exec(_ call: CAPPluginCall) {
         let command = call.getString("command") ?? ""
         self.execCall = call
-        MobileFFmpeg.executeAsync(command, withCallback: self)
-    }
-    
-    public func executeCallback(_ executionId: Int, _ returnCode: Int32) {
-        let output = MobileFFmpegConfig.getLastCommandOutput()
-        var responseToUser: String
-        var status: String
-        if (returnCode == RETURN_CODE_SUCCESS) {
-            responseToUser = "success out=\(output ?? "")"
-            status = "SUCCESS"
-        } else if (returnCode == RETURN_CODE_CANCEL) {
-            responseToUser = "canceled"
-            status = "CANCELED"
-        } else {
-            responseToUser = "failure code=\(returnCode) out=\(output ?? "")"
-            status = "ERROR"
+        DispatchQueue.main.async {
+            let vc = UIViewController()
+            vc.view.backgroundColor = .red
+            self.bridge.viewController.present(vc, animated: true, completion: nil)
+            
         }
-        guard let call = execCall else { return }
+        MobileFFmpeg.execute(command); // This line will cause compilation error
         call.success([
-            "result": responseToUser,
-            "returnCode": returnCode,
-            "status": status
+            "value": "test"
         ])
-        execCall = nil
     }
-    
-    
 }
+
+//extension FFmpeg: ExecuteDelegate {
+//    public func executeCallback(_ executionId: Int, _ returnCode: Int32) {
+//        let output = MobileFFmpegConfig.getLastCommandOutput()
+//        var responseToUser: String
+//        var status: String
+//        if (returnCode == RETURN_CODE_SUCCESS) {
+//            responseToUser = "success out=\(output ?? "")"
+//            status = "SUCCESS"
+//        } else if (returnCode == RETURN_CODE_CANCEL) {
+//            responseToUser = "canceled"
+//            status = "CANCELED"
+//        } else {
+//            responseToUser = "failure code=\(returnCode) out=\(output ?? "")"
+//            status = "ERROR"
+//        }
+//        guard let call = execCall else { return }
+//        call.success([
+//            "result": responseToUser,
+//            "returnCode": returnCode,
+//            "status": status
+//        ])
+//        execCall = nil
+//    }
+//}
